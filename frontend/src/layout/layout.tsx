42997bdf0b3e5f834270645c1419aaedbad19599
bananackck <emilly03141221@gmail.com>
2026-07-14 00:39:03 +0900

    Feature(FE): 인증 패널을 앱 레이아웃에 연결

--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -1,15 +1,18 @@
 import { AppHeader } from "../components/AppHeader";
+import { AuthProvider } from "../components/AuthProvider";
 import { LNB } from "../components/LNB";
 import { WorkspaceMain } from "../components/WorkspaceMain";
 
 export function AppLayout() {
   return (
-    <div className="mx-auto flex min-h-svh w-full flex-col bg-surface">
-      <AppHeader />
-      <div className="flex min-h-[calc(100svh-var(--spacing-app-header))] flex-1 flex-col md:flex-row">
-        <LNB />
-        <WorkspaceMain />
+    <AuthProvider>
+      <div className="mx-auto flex min-h-svh w-full flex-col bg-surface">
+        <AppHeader />
+        <div className="flex min-h-[calc(100svh-var(--spacing-app-header))] flex-1 flex-col md:flex-row">
+          <LNB />
+          <WorkspaceMain />
+        </div>
       </div>
-    </div>
+    </AuthProvider>
   );
 }
